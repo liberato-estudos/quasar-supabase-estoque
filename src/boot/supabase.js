c334db22b9f982382c911dfced090f39d9f5a9a6
Rafael <liberato@utfpr.edu.br>
2022-11-21 16:06:53 -0300

    configuração .env

--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -1,11 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
 import useAuthUser from 'src/composables/UseAuthUser'
 
-const supabaseUrl = 'https://xxboqoluuaowghbtyqfh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4Ym9xb2x1dWFvd2doYnR5cWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcyMjMzNzQsImV4cCI6MTk4Mjc5OTM3NH0.oR-XInHqTsSoaqVW_swqOFf0xfzr1EYzA2MvdQP-bAI'
+// const supabaseUrl = 'https://xxboqoluuaowghbtyqfh.supabase.co'
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4Ym9xb2x1dWFvd2doYnR5cWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcyMjMzNzQsImV4cCI6MTk4Mjc5OTM3NH0.oR-XInHqTsSoaqVW_swqOFf0xfzr1EYzA2MvdQP-bAI'
+
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-// console.log('init supabase', supabase)
 
 supabase.auth.onAuthStateChange((event, session) => {
   const { user } = useAuthUser()

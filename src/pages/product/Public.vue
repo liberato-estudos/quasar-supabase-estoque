5c747aa7ca99765d82cf0afea310e586ceed740a
Rafael <liberato@utfpr.edu.br>
2022-11-30 19:28:20 -0300

    Aula 17 criando url pública

--- src/pages/product/Public.vue
+++ src/pages/product/Public.vue
@@ -13,11 +13,17 @@
         :loading="loading"
         sortBy="name"
         grid
+        :filter="filter"
       >
 
         <template v-slot:top>
           <span class="text-h6">Produtos</span>
           <q-space />
+          <q-input outlined dense debounce="300" v-model="filter" placeholder="Procurar" class="q-mr-sm">
+            <template v-slot:append>
+              <q-icon name="mdi-magnify" />
+            </template>
+          </q-input>
         </template>
 
         <template v-slot:item="props">
@@ -26,7 +32,7 @@
               <q-img :src="props.row.img_url" :ratio="4/3" />
               <q-card-section class="text-center">
                 <div class="text-h6">{{ props.row.name }}</div>
-                <div class="text-subtitle">{{ props.row.price }}</div>
+                <div class="text-subtitle">{{ formatCurrency(props.row.price) }}</div>
               </q-card-section>
 
 
@@ -46,11 +52,13 @@ import useApi from 'src/composables/useApi';
 import { useRoute } from 'vue-router';
 import useNotify from 'src/composables/UseNotify';
 import { columnsProduct } from './table'
+import { formatCurrency } from 'src/utils/format'
 
 
 
 const products = ref([])
 const loading = ref(true)
+const filter = ref('')
 
 const table = "product"
 
